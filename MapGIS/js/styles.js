1fc7c8f4da7646cef319505e37f5c0b0edda4b16
qianjuanbing <your_email@example.com>
2025-05-09 17:30:59 +0800

    添加-WebGIS标注

--- MapGIS/js/styles.js
+++ MapGIS/js/styles.js
@@ -7,3 +7,48 @@ const yellowLine = new ol.style.Style({
     width: 2
   })
 })
+
+const icon = new ol.style.Style({
+  image: new ol.style.Icon({
+    anchor: [0.5, 60],
+    anchorOrigin: 'top-right',
+    anchorXUnits: 'fraction',
+    anchorYUnits: 'pixels',
+    offsetOrigin: 'top-right',
+    scale: 0.5,
+    opacity: 0.75,
+    src: '../image/blueIcon.png'
+  })
+})
+
+const text = new ol.style.Style({
+  text: new ol.style.Text({
+    offsetY: 10,
+    font: 'normal 14px 微软雅黑',
+    text: '北京',
+    fill: new ol.style.Fill({ color: '#aa3300', }),
+    stroke: new ol.style.Stroke({ color: '#ffcc33', width: 2 })
+
+  })
+})
+
+const label = new ol.style.Style({
+  image: new ol.style.Icon({
+    anchor: [0.5, 60],
+    anchorOrigin: 'top-right',
+    anchorXUnits: 'fraction',
+    anchorYUnits: 'pixels',
+    offsetOrigin: 'top-right',
+    scale: 0.5,
+    opacity: 0.75,
+    src: '../image/blueIcon.png'
+  }),
+  text: new ol.style.Text({
+    offsetY: 10,
+    font: 'normal 14px 微软雅黑',
+    text: '北京',
+    fill: new ol.style.Fill({ color: '#aa3300', }),
+    stroke: new ol.style.Stroke({ color: '#ffcc33', width: 2 })
+
+  })
+})
